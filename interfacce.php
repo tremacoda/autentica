01545ca41672d7c9c0035b110bc4c7bcb00a1345
tremacoda <mario.bergognoni@gmail.com>
2020-03-26 16:57:12 +0100

    aggiunto file di interf

---
+++ interfacce.php
@@ -0,0 +1,45 @@
+<?php
+class Employee {
+	private $code;
+	function setCode($code) {
+		if ($code == "") {
+			echo "Name cannot be blank!";
+		} else {
+			$this->code = $code;
+		}
+	}
+	function getCode() {
+		echo "<br />"."Code: ".$this->code."<br />";
+	}
+}
+interface Person {
+	// costanti
+	const COSTANTE = "valore";
+	// definizione dei metodi
+	public function getName();
+	public function setName($name, $lastname);
+}
+class Student extends Employee implements Person {
+	private $name = "";
+	private $lastname = "";
+	// implementazione dei metodi
+	public function getName(){
+		// corpo del Metodo
+		echo "$this->name $this->lastname";
+	}
+	public function setName($name, $lastname){
+		// corpo del Metodo
+		$this->name = $name;
+		$this->lastname = $lastname;
+	}
+    function setCode($code,$prefix) {
+		
+			$this->code =$prefix."/".$code;
+		}
+}
+$student = new Student();
+$student->setName( "Giulia", "Q" );
+$student->getName();
+$student->setCode("04022012","Plz.");
+$student->getCode();
+?>
